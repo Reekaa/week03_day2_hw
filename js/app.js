f2d43d6e557621120719910eeda60075be150b22
Reka <a9886177@9886177s-MacBook-Pro.local>
2019-04-16 16:12:35 +0100

    radom selection works

--- js/app.js
+++ js/app.js
@@ -6,6 +6,8 @@ document.addEventListener('DOMContentLoaded', () => {
       rockButton.addEventListener('click', (event) => {
         const playerParag = document.querySelector('#players-choice');
         playerParag.textContent = `Player plays: rock.`
+        const computerParag = document.querySelector('#computers-choice');
+        computerParag.textContent = `Computer plays: ${random(arrayOfButtons)}`;
       });
 
   const paperButton = document.querySelector('#paper');
@@ -13,6 +15,8 @@ document.addEventListener('DOMContentLoaded', () => {
       paperButton.addEventListener('click', (event) => {
         const playerParag = document.querySelector('#players-choice');
         playerParag.textContent = `Player plays: paper.`
+        const computerParag = document.querySelector('#computers-choice');
+        computerParag.textContent = `Computer plays: ${random(arrayOfButtons)}`;
       });
 
   const scisoorsButton = document.querySelector('#scissors');
@@ -20,12 +24,12 @@ document.addEventListener('DOMContentLoaded', () => {
       scisoorsButton.addEventListener('click', (event) => {
         const playerParag = document.querySelector('#players-choice');
         playerParag.textContent = `Player plays: scissors.`
+        const computerParag = document.querySelector('#computers-choice');
+        computerParag.textContent = `Computer plays: ${random(arrayOfButtons)}`;
       });
 
   const arrayOfButtons = ['rock', 'paper', 'scissors'];
   const random = (arrayOfButtons) => {
-  return arrayOfButtons[Math.floor((Math.random() * Math.floor(arrayOfButtons.length)))];
-};
-console.log(random(arrayOfButtons));
-
+    return arrayOfButtons[Math.floor((Math.random() * Math.floor(arrayOfButtons.length)))];
+  };
 });
